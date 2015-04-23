c74ebdc75b9fce7b4d2c15fe3b8f66a211879507
JamesVaughan <James.Vaughan@utoronto.ca>
2015-04-23 11:09:30 -0400

    Added the abbility to export EMME Worksheets.  Added stubs for countpost extractions.

---
+++ Code/TMG.Emme/Tools/Analysis/Traffic/ExportCountpostResults.cs
@@ -0,0 +1,68 @@
+﻿/*
+    Copyright 2015 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMG.Input;
+using XTMF;
+
+namespace TMG.Emme.Tools.Analysis.Traffic
+{
+    public class ExportCountpostResults : IEmmeTool
+    {
+        private const string ToolName = "tmg.analysis.traffic.export_screenline_results";
+        public string Name { get; set; }
+
+        public float Progress { get; set; }
+
+        public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
+
+        [RunParameter("Scenario Number", "1", typeof(int), "The scenario to interact with")]
+        public int ScenarioNumber;
+
+        [RunParameter("CountpostAttributeFlag", "@stn1", typeof(string), "The attribute name to use for identifying countposts.")]
+        public string CountpostAttributeFlag;
+
+        [RunParameter("AlternateCountpostAttributeFlag", "@stn2", typeof(string), "The alternate attribute name to use for identifying countposts.")]
+        public string AlternateCountpostAttributeFlag;
+
+
+        [SubModelInformation(Required = true, Description = "The location to save the results to")]
+        public FileLocation SaveTo;
+
+        public bool Execute(Controller controller)
+        {
+            var modeller = controller as ModellerController;
+            if(modeller == null)
+            {
+                throw new XTMFRuntimeException("In '" + Name + "' we require the use of EMME Modeller in order to execute.");
+            }
+            return false;// modeller.Run(ToolName, );
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            error = "'" + this.GetType().FullName + "' is not implemented";
+            return false;
+        }
+    }
+
+}
