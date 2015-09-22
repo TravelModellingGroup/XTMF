527a028ef6b5dd6e86ec81b3ef0262298c31ec5d
JamesVaughan <James.Vaughan@utoronto.ca>
2015-09-22 08:02:52 -0400

    Added the ability to get the end time for activities and record what modes they are leaving that activity from with the normal time bins.

--- Code/Tasha.Validation/ModeChoice/StartTimeDistributions.cs
+++ Code/Tasha.Validation/ModeChoice/StartTimeDistributions.cs
@@ -46,27 +46,30 @@ namespace Tasha.Validation.ModeChoice
         [RunParameter("Trip Start Time", true, "Set to true to get the trip's start time.  Setting this to false will give the activity start times.")]
         public bool TripStartTime;
 
+        [RunParameter("Use Origin Purpose", false, "Use the origin purpose instead of the destination purpose.  This will be similar to getting the end time for trips.")]
+        public bool UseOriginPurpose;
+
         private const int NumberOfTimeBins = 48;
         private ITashaMode[] Modes;
         public void Execute(ITashaHousehold household, int iteration)
         {
             var persons = household.Persons;
-            for(int i = 0; i < persons.Length; i++)
+            for (int i = 0; i < persons.Length; i++)
             {
-                if(persons[i].Age >= MinimumAge)
+                if (persons[i].Age >= MinimumAge)
                 {
                     var expansionFactor = persons[i].ExpansionFactor;
                     var tripChains = persons[i].TripChains;
-                    for(int j = 0; j < tripChains.Count; j++)
+                    for (int j = 0; j < tripChains.Count; j++)
                     {
                         var tripChain = tripChains[j].Trips;
-                        for(int k = 0; k < tripChain.Count; k++)
+                        for (int k = 0; k < tripChain.Count; k++)
                         {
                             int index = (((int)(TripStartTime ? tripChain[k].TripStartTime : tripChain[k].ActivityStartTime).ToMinutes()) / 30);
                             var tripStartIndex = index < 0 ? (index % NumberOfTimeBins + NumberOfTimeBins) % NumberOfTimeBins : index % NumberOfTimeBins;
-                            var array = GetPurposeCount(tripChain[k].Purpose);
+                            var array = GetPurposeCount(tripChain, k);
                             var tripModeIndex = GetTripModeIndex(tripChain[k].Mode);
-                            if(tripModeIndex >= 0)
+                            if (tripModeIndex >= 0)
                             {
                                 var row = array[tripStartIndex];
                                 lock (array[tripStartIndex])
@@ -82,9 +85,9 @@ namespace Tasha.Validation.ModeChoice
 
         private int GetTripModeIndex(ITashaMode mode)
         {
-            for(int i = 0; i < Modes.Length; i++)
+            for (int i = 0; i < Modes.Length; i++)
             {
-                if(mode == Modes[i])
+                if (mode == Modes[i])
                 {
                     return i;
                 }
@@ -95,15 +98,15 @@ namespace Tasha.Validation.ModeChoice
         private float[][] GetPurposeCount(Activity purpose)
         {
             float[][] ret;
-            if(!TimeBin.TryGetValue(purpose, out ret))
+            if (!TimeBin.TryGetValue(purpose, out ret))
             {
                 lock (TimeBin)
                 {
                     Thread.MemoryBarrier();
-                    if(!TimeBin.TryGetValue(purpose, out ret))
+                    if (!TimeBin.TryGetValue(purpose, out ret))
                     {
                         ret = new float[NumberOfTimeBins][];
-                        for(int i = 0; i < ret.Length; i++)
+                        for (int i = 0; i < ret.Length; i++)
                         {
                             ret[i] = new float[Modes.Length];
                         }
@@ -114,17 +117,31 @@ namespace Tasha.Validation.ModeChoice
             return ret;
         }
 
+        private float[][] GetPurposeCount(List<ITrip> chain, int index)
+        {
+            Activity purpose;
+            if (UseOriginPurpose)
+            {
+                purpose = index > 0 ? chain[index - 1].Purpose : Activity.Home;
+            }
+            else
+            {
+                purpose = chain[index].Purpose;
+            }
+            return GetPurposeCount(purpose);
+        }
+
         public void IterationFinished(int iteration)
         {
-            using(var writer = new StreamWriter(OutputFile, true))
+            using (var writer = new StreamWriter(OutputFile, true))
             {
                 writer.Write("Iteration: ");
                 writer.WriteLine(iteration + 1);
                 writer.Write("Time");
-                foreach(var key in TimeBin.Keys)
+                foreach (var key in TimeBin.Keys)
                 {
                     var name = Enum.GetName(typeof(Activity), key);
-                    for(int i = 0; i < Modes.Length; i++)
+                    for (int i = 0; i < Modes.Length; i++)
                     {
                         writer.Write(',');
                         writer.Write(name);
@@ -135,13 +152,13 @@ namespace Tasha.Validation.ModeChoice
                 writer.WriteLine();
                 Time thirtyMinutes = new Time() { Minutes = 30 };
                 Time currentTime = new Time();
-                for(int i = 0; i < NumberOfTimeBins; i++)
+                for (int i = 0; i < NumberOfTimeBins; i++)
                 {
                     writer.Write(currentTime);
-                    foreach(var key in TimeBin.Keys)
+                    foreach (var key in TimeBin.Keys)
                     {
                         var purposeData = GetPurposeCount(key)[i];
-                        for(int j = 0; j < purposeData.Length; j++)
+                        for (int j = 0; j < purposeData.Length; j++)
                         {
                             writer.Write(',');
                             writer.Write(purposeData[j]);
@@ -169,7 +186,7 @@ namespace Tasha.Validation.ModeChoice
         public void IterationStarting(int iteration)
         {
             TimeBin.Clear();
-            if(Modes == null)
+            if (Modes == null)
             {
                 Modes = Root.AllModes.ToArray();
             }
