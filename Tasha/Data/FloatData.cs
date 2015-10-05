3ee4bae530f4de73f4d8dc0d3263dd1a463c146d
JamesVaughan <James.Vaughan@utoronto.ca>
2015-10-05 16:28:23 -0400

    Added a module to store a float as a data source.

---
+++ Tasha/Data/FloatData.cs
@@ -0,0 +1,75 @@
+﻿/*
+    Copyright 2015 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Datastructure;
+using XTMF;
+using TMG;
+using TMG.Input;
+namespace Tasha.Data
+{
+    public class FloatData : IDataSource<float>
+    {
+        [RootModule]
+        public ITravelDemandModel Root;
+
+        private float Data;
+
+        public float GiveData()
+        {
+            return this.Data;
+        }
+
+        public bool Loaded
+        {
+            get; set;
+        }
+
+        public void LoadData()
+        {
+            Data = 0.0f;
+            Loaded = true;
+        }
+
+        public void UnloadData()
+        {
+            Data = 0.0f;
+            Loaded = false;
+        }
+
+        public string Name { get; set; }
+
+        public float Progress
+        {
+            get { return 0f; }
+        }
+
+        public Tuple<byte, byte, byte> ProgressColour
+        {
+            get { return null; }
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+    }
+}
