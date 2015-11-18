d214881f2fd1349396e36c5bb99a81341e46ea31
JamesVaughan <James.Vaughan@utoronto.ca>
2015-11-18 14:25:18 -0500

    Added a new module to perform an average of 2 matrices.

---
+++ Tasha/Data/AverageOD.cs
@@ -0,0 +1,158 @@
+﻿/*
+    Copyright 2015 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Datastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMG;
+using TMG.Functions;
+using XTMF;
+
+namespace Tasha.Data
+{
+    [ModuleInformation(
+        Description = "This module is designed to take the average of the two given matrices.  It can then store the result in the first matrix or create a new one to pass on to the next step."
+        )]
+    public class AverageOD : IDataSource<SparseTwinIndex<float>>
+    {
+        [RootModule]
+        public ITravelDemandModel Root;
+
+        public bool Loaded { get; set; }
+
+        public string Name { get; set; }
+
+        public float Progress { get; set; }
+
+        public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
+
+        private SparseTwinIndex<float> Data;
+
+        [SubModelInformation(Required = false, Description = "The first matrix if loading from a resource")]
+        public IResource FirstMatrix;
+
+        [SubModelInformation(Required = false, Description = "The second matrix if loading from a resource")]
+        public IResource SecondMatrix;
+
+        [SubModelInformation(Required = false, Description = "The first matrix if loading from a data source")]
+        public IDataSource<SparseTwinIndex<float>> FirstDataSource;
+
+        [SubModelInformation(Required = false, Description = "The second matrix if loading from a data source")]
+        public IDataSource<SparseTwinIndex<float>> SecondDataSource;
+
+        [RunParameter("Overwrite First", false, "Should we save memory by building the result in the first matrix?")]
+        public bool OverwriteFirst;
+
+        public SparseTwinIndex<float> GiveData()
+        {
+            return Data;
+        }
+
+        public void LoadData()
+        {
+            SparseTwinIndex<float> first = GetMatrix(FirstMatrix, FirstDataSource);
+            SparseTwinIndex<float> second = GetMatrix(SecondMatrix, SecondDataSource);
+            SparseTwinIndex<float> ret = GetResultMatrix(first);
+            var data = ret.GetFlatData();
+            var f = first.GetFlatData();
+            var s = second.GetFlatData();
+            if (VectorHelper.IsHardwareAccelerated)
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    VectorHelper.Average(data[i], 0, f[i], 0, s[i], 0, data[i].Length);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+                    for (int j = 0; j < data[i].Length; j++)
+                    {
+                        data[i][j] = (f[i][j] + s[i][j]) * 0.5f;
+                    }
+                }
+            }
+            Loaded = true;
+        }
+
+        private SparseTwinIndex<float> GetResultMatrix(SparseTwinIndex<float> first)
+        {
+            if (OverwriteFirst)
+            {
+                return first;
+            }
+            return Root.ZoneSystem.ZoneArray.CreateSquareTwinArray<float>();
+        }
+
+        private SparseTwinIndex<float> GetMatrix(IResource resource, IDataSource<SparseTwinIndex<float>> dataSource)
+        {
+            if (resource != null)
+            {
+                return resource.AquireResource<SparseTwinIndex<float>>();
+            }
+            dataSource.LoadData();
+            var ret = dataSource.GiveData();
+            dataSource.UnloadData();
+            return null;
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            if (!EnsureOne(FirstMatrix, FirstDataSource, "first", ref error))
+            {
+                return false;
+            }
+            if (!EnsureOne(SecondMatrix, SecondDataSource, "second", ref error))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool EnsureOne(IResource firstMatrix, IDataSource<SparseTwinIndex<float>> firstDataSource, string matrixName, ref string error)
+        {
+            if (firstMatrix != null && firstDataSource != null)
+            {
+                error = "In '" + Name + "' both matrices were defined for the " + matrixName + " matrix.  Only one can be filled out at the same time!";
+                return false;
+            }
+            if (firstMatrix == null && firstDataSource == null)
+            {
+                error = "In '" + Name + "' no matrices were defined for the " + matrixName + " matrix.  One must be filled out!";
+                return false;
+            }
+            if (firstMatrix != null && !firstMatrix.CheckResourceType<SparseTwinIndex<float>>())
+            {
+                error = "In '" + Name + "' the " + matrixName + " matrix resource was not of type IDataSource<SparseTwinIndex<float>>!";
+                return false;
+            }
+            return true;
+        }
+
+        public void UnloadData()
+        {
+            Data = null;
+            Loaded = false;
+        }
+    }
+
+}
