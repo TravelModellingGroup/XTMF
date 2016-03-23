1da2bf37c3a21615de3ea600364dc0d8a907f8f5
JamesVaughan <James.Vaughan@utoronto.ca>
2016-03-23 17:26:14 -0400

    Added an additional module that allows you to copy scenarios within an EMME databank.

---
+++ Code/TMG.Emme/XTMF_Internal/CopyScenario.cs
@@ -0,0 +1,65 @@
+﻿/*
+    Copyright 2016 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Datastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XTMF;
+
+namespace TMG.Emme.XTMF_Internal
+{
+    [ModuleInformation(
+        Description = "This tool will allow you to be able to copy a scenario to a given other scenario number."
+        )]
+    public class CopyScenario : IEmmeTool
+    {
+        [RunParameter("Base Scenario", 1, "The scenario to copy from.")]
+        public int BaseScenario;
+
+        [RunParameter("Base Scenario", 2, "The scenario to copy to.")]
+        public int DestinationScenario;
+
+        [RunParameter("Copy Assignments", false, "Copy the results from assignments as well.")]
+        public bool CopyAssignments;
+
+        public string Name { get; set; }
+
+        public float Progress { get; set; }
+
+        public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
+
+        public bool Execute(Controller controller)
+        {
+            var mc = controller as ModellerController;
+            if (mc == null)
+            {
+                throw new XTMFRuntimeException("Controller is not a ModellerController!");
+            }
+            return mc.Run("tmg.XTMF_Internal.copy_scenario", string.Join(" ", BaseScenario, DestinationScenario, CopyAssignments));
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+    }
+
+}
