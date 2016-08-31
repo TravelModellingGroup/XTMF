5974b65b4f76ce3a3d820122adfcadef174721a1
JamesVaughan <James.Vaughan@utoronto.ca>
2016-08-31 11:21:04 -0400

    Fixed path for executing a run from the GUI.

--- Code/XTMF.Gui.1.1/MainWindow.xaml.cs
+++ Code/XTMF.Gui.1.1/MainWindow.xaml.cs
@@ -199,73 +199,11 @@ namespace XTMF.Gui
             }
         }
 
-        private void SetupRunButton(LayoutDocument document)
-        {
-            var modelSystem = document.Content as ModelSystemDisplay;
-            if (modelSystem != null)
-            {
-                var session = modelSystem.Session;
-                if (session.CanRun)
-                {
-                    _CurrentRun = () =>
-                    {
-                        var runName = "Run Name";
-                        StringRequest req = new StringRequest("Run Name", ValidateName);
-                        var trueWindow = Window.GetWindow(document.Content as DependencyObject);
-                        var testWindow = GetWindow(document.Content as DependencyObject);
-                        var vis = document.Content as UserControl;
-                        if (vis != null && testWindow != trueWindow)
-                        {
-                            var topLeft = vis.PointToScreen(new Point());
-                            // Since the string request dialog isn't shown yet we need to use some defaults as width and height are not available.
-                            req.Left = topLeft.X + ((vis.ActualWidth - StringRequest.DefaultWidth) / 2);
-                            req.Top = topLeft.Y + ((vis.ActualHeight - StringRequest.DefaultHeight) / 2);
-                        }
-                        else
-                        {
-                            req.Owner = trueWindow;
-                        }
-                        if (req.ShowDialog() == true)
-                        {
-                            runName = req.Answer;
-                            string error = null;
-                            if (!RunAlreadyExists(runName, session) || MessageBox.Show("This run name has been previously used.  Continue?", "Continue?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.Yes)
-                            {
-                                var run = session.Run(runName, ref error);
-                                if (run != null)
-                                {
-                                    RunWindow window = new RunWindow(session, run, runName);
-                                    var doc = AddNewWindow("New Run", window);
-                                    doc.Closing += (o, e) =>
-                                    {
-                                        if (!window.CloseRequested())
-                                        {
-                                            e.Cancel = true;
-                                            return;
-                                        }
-                                    };
-                                    doc.CanClose = true;
-                                    doc.IsSelected = true;
-                                    Keyboard.Focus(window);
-                                    window.Focus();
-                                }
-                                else
-                                {
-                                    MessageBox.Show(this, error, "Unable to run", MessageBoxButton.OK, MessageBoxImage.Error);
-                                }
-                            }
-                        }
-                    };
-                }
-            }
-        }
-
         private bool RunAlreadyExists(string runName, ModelSystemEditingSession session)
         {
             return session.RunNameExists(runName);
         }
 
-        private Action _CurrentRun;
         private LayoutDocument CurrentDocument;
 
         private void SaveMenu_Click(object sender, RoutedEventArgs e)
@@ -609,7 +547,58 @@ namespace XTMF.Gui
 
         public void ExecuteRun()
         {
-            _CurrentRun?.Invoke();
+            var document = CurrentDocument;
+            var modelSystem = document.Content as ModelSystemDisplay;
+            if (modelSystem != null)
+            {
+                var session = modelSystem.Session;
+                var runName = "Run Name";
+                StringRequest req = new StringRequest("Run Name", ValidateName);
+                var trueWindow = Window.GetWindow(document.Content as DependencyObject);
+                var testWindow = GetWindow(document.Content as DependencyObject);
+                var vis = document.Content as UserControl;
+                if (vis != null && testWindow != trueWindow)
+                {
+                    var topLeft = vis.PointToScreen(new Point());
+                    // Since the string request dialog isn't shown yet we need to use some defaults as width and height are not available.
+                    req.Left = topLeft.X + ((vis.ActualWidth - StringRequest.DefaultWidth) / 2);
+                    req.Top = topLeft.Y + ((vis.ActualHeight - StringRequest.DefaultHeight) / 2);
+                }
+                else
+                {
+                    req.Owner = trueWindow;
+                }
+                if (req.ShowDialog() == true)
+                {
+                    runName = req.Answer;
+                    string error = null;
+                    if (!RunAlreadyExists(runName, session) || MessageBox.Show("This run name has been previously used.  Continue?", "Continue?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.Yes)
+                    {
+                        var run = session.Run(runName, ref error);
+                        if (run != null)
+                        {
+                            RunWindow window = new RunWindow(session, run, runName);
+                            var doc = AddNewWindow("New Run", window);
+                            doc.Closing += (o, e) =>
+                            {
+                                if (!window.CloseRequested())
+                                {
+                                    e.Cancel = true;
+                                    return;
+                                }
+                            };
+                            doc.CanClose = true;
+                            doc.IsSelected = true;
+                            Keyboard.Focus(window);
+                            window.Focus();
+                        }
+                        else
+                        {
+                            MessageBox.Show(this, error, "Unable to run", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
         }
 
         internal void CloseWindow(UIElement window)
