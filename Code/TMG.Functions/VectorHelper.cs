b62e28fac0d3abf5469f984e25bf01aab6423342
JamesVaughan <James.Vaughan@utoronto.ca>
2016-12-14 16:18:59 -0500

    Added backend functions to apply and and or for flags in ODMath.

--- Code/TMG.Functions/VectorHelper.cs
+++ Code/TMG.Functions/VectorHelper.cs
@@ -2455,6 +2455,251 @@ namespace TMG.Functions
             }
         }
 
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagAnd(float[] dest, float value, float[] data)
+        {
+            // check if we are supposed to just clear everything and use a faster function for that
+            if (value == 0.0f)
+            {
+                Array.Clear(dest, 0, dest.Length);
+            }
+            else
+            {
+                if (Vector.IsHardwareAccelerated)
+                {
+                    int i;
+                    if (dest.Length != data.Length)
+                    {
+                        throw new ArgumentException("The size of the arrays are not the same!", nameof(dest));
+                    }
+                    Vector<float> zero = Vector<float>.Zero;
+                    Vector<float> vValue = new Vector<float>(value);
+                    for (i = 0; i < data.Length - Vector<float>.Count; i++)
+                    {
+                        var vData = new Vector<float>(data, i);
+                        Vector.ConditionalSelect(Vector.Equals(vData, zero), zero, vValue).CopyTo(dest, i);
+                    }
+                    for (; i < data.Length; i++)
+                    {
+                        dest[i] = data[i] == 0 ? 0.0f : value;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        dest[i] = data[i] == 0 ? 0.0f : value;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagAnd(float[] destination, int destIndex, float[] lhs, int lhsIndex, float[] rhs, int rhsIndex, int length)
+        {
+            if (Vector.IsHardwareAccelerated)
+            {
+                Vector<float> zero = Vector<float>.Zero;
+                if ((destIndex | lhsIndex | rhsIndex) == 0)
+                {
+                    int i = 0;
+                    for (; i <= length - Vector<float>.Count; i += Vector<float>.Count)
+                    {
+                        var f = new Vector<float>(lhs, i);
+                        var s = new Vector<float>(rhs, i);
+                        Vector.ConditionalSelect(Vector.Equals(f, zero), zero, s).CopyTo(destination, i);
+                    }
+                    // copy the remainder
+                    for (; i < length; i++)
+                    {
+                        destination[i] = lhs[i] == 0.0f ? 0.0f : rhs[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i <= length - Vector<float>.Count; i += Vector<float>.Count)
+                    {
+                        Vector.ConditionalSelect(Vector.Equals(new Vector<float>(lhs, i + lhsIndex), zero), zero, new Vector<float>(rhs, i + rhsIndex))
+                            .CopyTo(destination, i + destIndex);
+                    }
+                    // copy the remainder
+                    for (int i = length - (length % Vector<float>.Count); i < length; i++)
+                    {
+                        destination[i + destIndex] = lhs[i + lhsIndex] == 0.0f ? 0.0f : rhs[i + rhsIndex];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    destination[i + destIndex] = lhs[i + lhsIndex] == 0.0f ? 0.0f : rhs[i + rhsIndex];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagAnd(float[][] dest, float[][] data, float literalValue)
+        {
+            for (int i = 0; i < dest.Length; i++)
+            {
+                FlagAnd(dest[i], data[i], literalValue);
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagAnd(float[] dest, float[] data, float literalValue)
+        {
+            FlagAnd(dest, literalValue, data);
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagAnd(float[][] dest, float[][] lhs, float[][] rhs)
+        {
+            for (int i = 0; i < dest.Length; i++)
+            {
+                FlagAnd(dest[i], 0, lhs[i], 0, rhs[i], 0, dest.Length);
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagAnd(float[][] v1, float literalValue, float[][] v2)
+        {
+            for (int i = 0; i < v1.Length; i++)
+            {
+                FlagAnd(v1[i], literalValue, v2[i]);
+            }
+        }
+
+
+        /////////////////////////
+
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagOr(float[] dest, float value, float[] data)
+        {
+            // check if we are supposed to just copy everything and use a faster function for that
+            if (value == 0.0f)
+            {
+                Array.Copy(dest, 0, data, 0, dest.Length);
+            }
+            else
+            {
+                // the vector implementation performed faster than the serial version by multiples
+                int i = 0;
+                var one = Vector<float>.One;
+                for (; i <= dest.Length - Vector<float>.Count; i += Vector<float>.Count)
+                {
+                    one.CopyTo(dest, i);
+                }
+                // copy the remainder
+                for (; i < dest.Length; i++)
+                {
+                    dest[i] = 1.0f;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagOr(float[] destination, int destIndex, float[] lhs, int lhsIndex, float[] rhs, int rhsIndex, int length)
+        {
+            if (Vector.IsHardwareAccelerated)
+            {
+                var one = Vector<float>.One;
+                if ((destIndex | lhsIndex | rhsIndex) == 0)
+                {
+                    int i = 0;
+                    for (; i <= length - Vector<float>.Count; i += Vector<float>.Count)
+                    {
+                        var f = new Vector<float>(lhs, i);
+                        var s = new Vector<float>(rhs, i);
+                        Vector.ConditionalSelect(Vector.Equals(f, one), one, s).CopyTo(destination, i);
+                    }
+                    // copy the remainder
+                    for (; i < length; i++)
+                    {
+                        destination[i] = lhs[i] == 1.0f ? 1.0f : rhs[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i <= length - Vector<float>.Count; i += Vector<float>.Count)
+                    {
+                        Vector.ConditionalSelect(Vector.Equals(new Vector<float>(lhs, i + lhsIndex), one), one, new Vector<float>(rhs, i + rhsIndex))
+                            .CopyTo(destination, i + destIndex);
+                    }
+                    // copy the remainder
+                    for (int i = length - (length % Vector<float>.Count); i < length; i++)
+                    {
+                        destination[i + destIndex] = lhs[i + lhsIndex] == 1.0f ? 1.0f : rhs[i + rhsIndex];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    destination[i + destIndex] = lhs[i + lhsIndex] == 1.0f ? 1.0f : rhs[i + rhsIndex];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagOr(float[][] dest, float[][] data, float literalValue)
+        {
+            for (int i = 0; i < dest.Length; i++)
+            {
+                FlagOr(dest[i], data[i], literalValue);
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagOr(float[] dest, float[] data, float literalValue)
+        {
+            FlagOr(dest, literalValue, data);
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagOr(float[][] dest, float[][] lhs, float[][] rhs)
+        {
+            for (int i = 0; i < dest.Length; i++)
+            {
+                FlagOr(dest[i], 0, lhs[i], 0, rhs[i], 0, dest.Length);
+            }
+        }
+
+        /// <summary>
+        /// Set the value to one if the condition is met.
+        /// </summary>
+        public static void FlagOr(float[][] v1, float literalValue, float[][] v2)
+        {
+            for (int i = 0; i < v1.Length; i++)
+            {
+                FlagOr(v1[i], literalValue, v2[i]);
+            }
+        }
 
         /// <summary>
         /// 
