5cb53ff8035092268e4a6103756eba614ebc26cd
JamesVaughan <James.Vaughan@utoronto.ca>
2017-02-10 15:59:40 -0500

    Fixed class name for TTFs.

--- Code/GTAModel/NetworkAssignment/V4 FBTA.cs
+++ Code/GTAModel/NetworkAssignment/V4 FBTA.cs
@@ -145,10 +145,10 @@ namespace TMG.GTAModel.NetworkAssignment
         private const string ToolName = "tmg.assignment.transit.V4_FBTA";
 
 
-        public sealed class TtfDefinitions : IModule
+        public sealed class TTFDefinitions : IModule
         {
             [RunParameter("TTF", 0, "The TTF number to assign to. 1 would mean TTF1.")]
-            public int TtfNumber;
+            public int TTFNumber;
 
             [RunParameter("Congestion Perception", 0.0f, "The congestion exponent to apply to this TTF.")]
             public float CongestionPerception;
@@ -169,7 +169,7 @@ namespace TMG.GTAModel.NetworkAssignment
         }
 
         [SubModelInformation(Description = "The TTF's to apply in the assignment.")]
-        public TtfDefinitions[] TTF;
+        public TTFDefinitions[] TTF;
 
 
         public bool Execute(Controller controller)
@@ -209,7 +209,7 @@ namespace TMG.GTAModel.NetworkAssignment
                                         Controller.ToEmmeFloat(ConnectorLogitScale),
                                         ExtractCongestedInVehicleTimeFlag,
                                         string.Join(",", from ttf in TTF
-                                                         select ttf.TtfNumber + ":"
+                                                         select ttf.TTFNumber + ":"
                                                          + Controller.ToEmmeFloat(ttf.CongestionPerception) + ":"
                                                          + Controller.ToEmmeFloat(ttf.CongestionExponent)),
                                         ImpedanceMatrix,
