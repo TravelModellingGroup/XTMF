1eead0b632f48a23afbb4f8013512d66c77acd15
BrendanReilly <brendan.reilly@utoronto.ca>
2017-02-16 13:01:08 -0500

    modelsystemdisplay incremental changes

---
+++ Code/XTMF.Gui.1.1/UserControls/ModuleTreeViewItem.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace XTMF.Gui.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ModuleTreeViewItem.xaml
+    /// </summary>
+    public partial class ModuleTreeViewItem : UserControl
+    {
+
+        public static readonly DependencyProperty ModuleTypeDependencyProperty =
+  DependencyProperty.Register("ModuleType",
+      typeof(ModuleType), typeof(ModuleTreeViewItem),
+          new PropertyMetadata(null));
+
+
+        
+        public ModuleTreeViewItem()
+        {
+            InitializeComponent();
+        }
+
+
+        public ModuleType ModuleType
+        {
+
+            get { return (ModuleType)GetValue(ModuleTypeDependencyProperty); }
+
+            set { SetValue(ModuleTypeDependencyProperty,value);}
+        }
+    }
+
+    public enum ModuleType
+    {
+        Optional, Meta, Required
+    }
+}
