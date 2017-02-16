4b27ff926a6765251decd7251b13f526da2d8be6
BrendanReilly <brendan.reilly@utoronto.ca>
2017-02-16 14:19:30 -0500

    modelsystemdisplay using proper treeviewitems

--- Code/XTMF.Gui.1.1/UserControls/ModuleTreeViewItem.xaml.cs
+++ Code/XTMF.Gui.1.1/UserControls/ModuleTreeViewItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace XTMF.Gui.UserControls
 {
@@ -15,7 +16,40 @@ namespace XTMF.Gui.UserControls
           new PropertyMetadata(null));
 
 
-        
+        public static readonly DependencyProperty IsSelectedDependencyProperty =
+         DependencyProperty.Register("IsSelected",
+             typeof(bool), typeof(ModuleTreeViewItem),
+                 new PropertyMetadata(true));
+
+
+
+        public static readonly DependencyProperty TitleTextDependencyProperty =
+
+            DependencyProperty.Register("TitleText",
+            typeof(string), typeof(ModuleTreeViewItem),
+                new PropertyMetadata(null));
+
+
+        public static readonly DependencyProperty SubTextDependencyProperty =
+        DependencyProperty.Register("SubText",
+            typeof(string), typeof(ModuleTreeViewItem),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty BitmapIconDependencyProperty =
+        DependencyProperty.Register("IsBitmapIcon",
+            typeof(bool), typeof(ModuleTreeViewItem),
+                new PropertyMetadata(false));
+
+        public static readonly DependencyProperty PathIconDependencyProperty =
+        DependencyProperty.Register("IsPathIcon",
+            typeof(bool), typeof(ModuleTreeViewItem),
+                new PropertyMetadata(true));
+
+        public static readonly DependencyProperty IconPathDependencyProperty =
+       DependencyProperty.Register("IconPath",
+           typeof(Path), typeof(ModuleTreeViewItem),
+               new PropertyMetadata(null));
+
         public ModuleTreeViewItem()
         {
             InitializeComponent();
@@ -29,10 +63,73 @@ namespace XTMF.Gui.UserControls
 
             set { SetValue(ModuleTypeDependencyProperty,value);}
         }
+
+        public bool IsSelected
+        {
+            get { return (bool)this.GetValue(IsSelectedDependencyProperty); }
+            set
+            {
+
+                this.SetValue(IsSelectedDependencyProperty, value);
+            }
+        }
+
+        public Path IconPath
+        {
+            get { return (Path)this.GetValue(IconPathDependencyProperty); }
+            set { this.SetValue(IconPathDependencyProperty, value); }
+        }
+
+        public bool IsBitmapIcon
+        {
+            get { return (bool)this.GetValue(BitmapIconDependencyProperty); }
+            set
+            {
+
+                this.SetValue(BitmapIconDependencyProperty, value);
+            }
+        }
+
+    
+
+
+
+        public bool IsPathIcon
+        {
+            get { return (bool)this.GetValue(PathIconDependencyProperty); }
+            set
+            {
+
+                this.SetValue(PathIconDependencyProperty, value);
+            }
+        }
+
+        public string TitleText
+        {
+            get { return (string)this.GetValue(TitleTextDependencyProperty); }
+            set
+            {
+
+                this.SetValue(TitleTextDependencyProperty, value);
+                this.Title.Content = value;
+            }
+        }
+
+        public string SubText
+        {
+            get { return (string)this.GetValue(SubTextDependencyProperty); }
+            set
+            {
+
+                this.SetValue(SubTextDependencyProperty, value);
+            }
+        }
     }
 
     public enum ModuleType
     {
         Optional, Meta, Required
     }
+
+
 }
