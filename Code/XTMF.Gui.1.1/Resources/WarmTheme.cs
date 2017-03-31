bbc8b195579d03916ca4583ee4a63c523b927099
BrendanReilly <brendan.reilly@utoronto.ca>
2017-03-31 13:09:34 -0400

    Warm Theme added

---
+++ Code/XTMF.Gui.1.1/Resources/WarmTheme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Wpf.AvalonDock.Themes;
+
+namespace XTMF.Gui.Resources
+{
+    public class WarmTheme : Theme
+    {
+        public override Uri GetResourceUri()
+        {
+            return new Uri(
+                "/XTMF.Gui;component/Resources/WarmTheme.xaml",
+                UriKind.Relative);
+        }
+    }
+}
