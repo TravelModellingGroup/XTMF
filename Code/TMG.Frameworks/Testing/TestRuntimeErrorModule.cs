2665eaf2ee30d0671aa3f7e909c32668cf030809
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-02-14 08:47:48 -0500

    Added testing module for RuntimeErrors

---
+++ Code/TMG.Frameworks/Testing/TestRuntimeErrorModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XTMF;
+
+namespace TMG.Frameworks
+{
+    class TestRuntimeErrorModule : ISelfContainedModule
+    {
+        public string Name { get; set; } = "TestRuntimeErrorModule";
+        public float Progress { get; }
+        public Tuple<byte, byte, byte> ProgressColour { get; }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+
+        public void Start()
+        {
+            throw new GenericRuntimeError();
+        }
+    }
+
+    class GenericRuntimeError : Exception
+    {
+
+    }
+}
