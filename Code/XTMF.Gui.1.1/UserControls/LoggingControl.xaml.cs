1a8a3352bd78031ea8ab11368620d80a83e1cec3
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-05-31 15:59:25 -0400

    Misc. license header updates GUI now targes .NET 4.6.1 to meet requirements for Logging framework, which has been added as a dependency Skeleton Logging control added

---
+++ Code/XTMF.Gui.1.1/UserControls/LoggingControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿/*
+    Copyright 2014-2015 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XTMF.Gui.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LoggingControl.xaml
+    /// </summary>
+    public partial class LoggingControl : UserControl
+    {
+        public LoggingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
