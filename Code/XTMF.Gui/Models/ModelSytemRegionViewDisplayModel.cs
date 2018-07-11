3bf90698636e7095d87df93fd26a8b33467690da
Brendan Reilly <brendan.benting@gmail.com>
2018-07-11 16:06:06 -0400

    Region View Updates -Added a new display model for Model System Display's RegionView

---
+++ Code/XTMF.Gui/Models/ModelSytemRegionViewDisplayModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTMF.Gui.Models
+{
+    /// <summary>
+    /// Display Model for ModelSystemDisplay's "Region View" mode
+    /// </summary>
+    class ModelSytemRegionViewDisplayModel
+    {
+        public ModelSytemRegionViewDisplayModel()
+        {
+
+        }
+    }
+}
