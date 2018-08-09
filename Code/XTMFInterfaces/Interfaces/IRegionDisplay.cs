9d0485c6069a9bd397fa3f6f8340b63e37ef3930
Brendan Reilly <brendan.benting@gmail.com>
2018-08-09 10:03:12 -0400

    Added RegionDisply and RegionGroup interfaces

---
+++ Code/XTMFInterfaces/Interfaces/IRegionDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTMF.Interfaces
+{
+    interface IRegionDisplay
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        List<IRegionGroup> RegionGroups { get; set; }
+    }
+}
---
+++ Code/XTMFInterfaces/Interfaces/IRegionGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTMF.Interfaces
+{
+    interface IRegionGroup
+    {
+        string Name { get; set; }
+
+        List<IModelSystemStructure> Modules { get; set; }
+    }
+}
