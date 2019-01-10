1d01edf02b832dc94f3f86207c4f03bf8d6767cd
Brendan Reilly <brendan.benting@gmail.com>
2019-01-10 14:18:37 -0500

    Added small assembly description to GTAModel project. Updated 'development debug remote host' build output location for several projects. Re-ordered ProjectDisplay's menu - new model system button is now first in the list.

--- Code/GTAModel/Properties/AssemblyInfo.cs
+++ Code/GTAModel/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "GTAModel" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "GTAModel related modules." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Microsoft" )]
 [assembly: AssemblyProduct( "GTAModel" )]
