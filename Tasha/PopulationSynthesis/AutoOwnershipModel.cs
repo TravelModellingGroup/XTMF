8a625250d79be1f784db2fcc5d9eb0e15167c444
JamesVaughan <James.Vaughan@utoronto.ca>
2019-03-27 10:54:21 -0400

    Added stub modules for auto ownership and driver's license.

---
+++ Tasha/PopulationSynthesis/AutoOwnershipModel.cs
@@ -0,0 +1,57 @@
+﻿/*
+    Copyright 2014-2019 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasha.Common;
+using TMG;
+using XTMF;
+
+namespace Tasha.PopulationSynthesis
+{
+    [ModuleInformation(Description = "The Auto Ownership model for GTAModel V4.1.0.")]
+    public sealed class AutoOwnershipModel : ICalculation<ITashaHousehold, int>
+    {
+        public string Name { get; set; }
+
+        public float Progress => 0f;
+
+        public Tuple<byte, byte, byte> ProgressColour => new Tuple<byte, byte, byte>(50,150,50);
+
+        public void Load()
+        {
+        }
+
+        public int ProduceResult(ITashaHousehold data)
+        {
+            return 0;
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+
+        public void Unload()
+        {
+        }
+    }
+}
