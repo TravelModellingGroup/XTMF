5548f1f2a22f815d30861083a8fde601d053e028
JamesVaughan <James.Vaughan@utoronto.ca>
2020-06-24 05:39:55 -0400

    Created interface for arbitrary parking information. Created parking model that can handle a day and night cycle of parking data with hourly and maximums by zone fed in by CSV.

---
+++ Tasha/Common/ParkingModel.cs
@@ -0,0 +1,123 @@
+﻿using Datastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMG;
+using TMG.Input;
+using XTMF;
+
+namespace Tasha.Common
+{
+    [ModuleInformation(Description = "Parking Model for GTAModel V4.2+.  All costs are in dollars, all times are in minutes-from-midnight.")]
+    public class ParkingModel : IParkingCost
+    {
+
+        [RootModule]
+        public ITravelDemandModel Root;
+
+        public bool Loaded { get; set; }
+
+        public string Name { get; set; }
+
+        public float Progress => 0f;
+
+        public Tuple<byte, byte, byte> ProgressColour => new Tuple<byte, byte, byte>(50, 150, 50);
+
+        private struct TimePeriodData
+        {
+            internal float StartOfPeriod;
+            internal float Hourly;
+            internal float Max;
+        }
+
+        private struct ParkingInformation
+        {
+            internal TimePeriodData Daily;
+            internal TimePeriodData Nightly;
+        }
+        
+        private SparseArray<IZone> _zoneSystem;
+        private ParkingInformation[] _parkingInformation;
+
+        [SubModelInformation(Required = true, Description = "CSV of (Zone,StartOfDay,DailyHourly,DailyMax,StartOfNight,NightlyHourly,NightlyMax)")]
+        public FileLocation ParkingData;
+
+        public float ComputeParkingCost(Time parkingStart, Time parkingEnd, IZone zone)
+        {
+            var zoneIndex = _zoneSystem.GetFlatIndex(zone.ZoneNumber);
+            if (zoneIndex >= 0)
+            {
+                var ps = parkingStart.ToMinutes();
+                var pe = parkingEnd.ToMinutes();
+                ref var zoneData = ref _parkingInformation[zoneIndex];
+                float cost = 0.0f;
+                /*
+                 * Parking has 3 components, time spent before the daily, during the daily, and during the nightly
+                 */
+                if(ps <= zoneData.Daily.StartOfPeriod)
+                {
+                    var duration = Math.Min(zoneData.Daily.StartOfPeriod, pe) - ps;
+                    cost = Math.Min(duration * zoneData.Nightly.Hourly, zoneData.Nightly.Max);
+                }
+                if(ps <= zoneData.Nightly.StartOfPeriod)
+                {
+                    var duration = Math.Min(pe, zoneData.Nightly.StartOfPeriod) - Math.Max(ps, zoneData.Daily.StartOfPeriod);
+                    cost += Math.Min(duration * zoneData.Daily.Hourly, zoneData.Daily.Max);
+                }
+                if(pe >= zoneData.Nightly.StartOfPeriod)
+                {
+                    var duration = pe - Math.Max(ps, zoneData.Nightly.StartOfPeriod);
+                    cost += Math.Min(duration * zoneData.Nightly.Hourly, zoneData.Nightly.Max);
+                }
+                return cost;
+            }
+            else
+            {
+                return 0f;
+            }
+        }
+
+        public void LoadData()
+        {
+            _zoneSystem = Root.ZoneSystem.ZoneArray;
+            _parkingInformation = new ParkingInformation[_zoneSystem.Count];
+            using(CsvReader reader = new CsvReader(ParkingData))
+            {
+                reader.LoadLine();
+                while(reader.LoadLine(out var columns))
+                {
+                    if (columns >= 7)
+                    {
+                        ParkingInformation data;
+                        reader.Get(out int zoneNumber, 0);
+                        var index = _zoneSystem.GetFlatIndex(zoneNumber);
+                        if (index < 0)
+                        {
+                            throw new XTMFRuntimeException(this, $"Unknown zone number {zoneNumber} when reading parking data!");
+                        }
+                        reader.Get(out data.Daily.StartOfPeriod, 1);
+                        reader.Get(out data.Daily.Hourly, 2);
+                        reader.Get(out data.Daily.Max, 3);
+                        reader.Get(out data.Nightly.StartOfPeriod, 4);
+                        reader.Get(out data.Nightly.Hourly, 5);
+                        reader.Get(out data.Nightly.Max, 6);
+                        _parkingInformation[index] = data;
+                    }
+                }
+            }
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+
+        public void UnloadData()
+        {
+            _zoneSystem = null;
+            _parkingInformation = null;
+        }
+    }
+}
---
+++ TashaInterfaces/Common/IParkingCost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMG;
+using XTMF;
+
+namespace Tasha.Common
+{
+    /// <summary>
+    /// This interface is used to build models that can calculate parking costs
+    /// </summary>
+    public interface IParkingCost : IDataSource
+    {
+        float ComputeParkingCost(Time parkingStart, Time parkingEnd, IZone zone);
+    }
+}
