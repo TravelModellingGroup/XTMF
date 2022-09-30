c16b5dd1d431f63f060e277dfdebc52997feea2d
James Vaughan <phibred@hotmail.com>
2022-09-30 10:11:50 -0400

    Update GUI and assembly version numbers. (#147)

--- Code/XTMF/Properties/AssemblyInfo.cs
+++ Code/XTMF/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("XTMF1.9")]
+[assembly: AssemblyProduct("XTMF1.11")]
 [assembly: AssemblyCopyright("University of Toronto 2011-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
