26b435c7a4ece046642ae7037cc430711ff4dcae
wdiogu <wdiogu@gmail.com>
2022-11-08 07:15:25 -0500

    fixed road assignment within subarea tool

--- Code/TMG.Emme/ExportSubareaTool.cs
+++ Code/TMG.Emme/ExportSubareaTool.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
+﻿/*
+    Copyright 2022 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Datastructure;
+using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMG.Emme;
-using XTMF;
 using TMG.Input;
-
+using XTMF;
 
 namespace TMG.Emme
 {
@@ -24,144 +44,97 @@ namespace TMG.Emme
         [RunParameter("Scenario Number", 0, "The scenario number to execute against.")]
         public int ScenarioNumber;
 
-        [SubModelInformation(Required = false, Description = "A link to the shapefile to specify boundary of the subarea")]
-        public FileLocation ShapefileLocation;
+        const string ToolName = "tmg.input_output.export_subarea_tool";
 
-        [SubModelInformation(Required = true, Description = "A link to the folder to output the subarea database")]
-        public FileLocation SubareaOutputFolder;
+        [SubModelInformation(Description = "The classes for this multi-class assignment.")]
+        public Class[] Classes;
 
-        [RunParameter("Extract Transit", true, "Whether or not the tool will extract out transit line and demand information from the scenario")]
-        public bool ExtractTransit;
+        [RunParameter("Peak Hour Factor", 0f, "A factor to apply to the demand in order to build a representative hour.")]
+        public float PeakHourFactor;
 
-        [RunParameter("Subarea Node Attribute","None", "The node attribute that will be used to define the subarea. Can be specified along with a shapefile or by itself. One of the shapefile or an node attribute must be defined")]
-        public string SubareaNodeAttribute;
+        [RunParameter("Iterations", 0, "The maximum number of iterations to run.")]
+        public int Iterations;
 
-        public string GetAttribute(string at)
+        [RunParameter("Relative Gap", 0f, "The minimum gap required to terminate the algorithm.")]
+        public float RelativeGap;
+
+        [RunParameter("Best Relative Gap", 0f, "The minimum gap required to terminate the algorithm.")]
+        public float BestRelativeGap;
+
+        [RunParameter("Normalized Gap", 0f, "The minimum gap required to terminate the algorithm.")]
+        public float NormalizedGap;
+
+        [RunParameter("Performance Mode", true, "Set this to false to leave a free core for other work, recommended to leave set to true.")]
+        public bool PerformanceMode;
+
+        [RunParameter("Run Title", "Multi-class Run", "The name of the run to appear in the logbook.")]
+        public string RunTitle;
+
+        [RunParameter("Background Transit", true, "Set this to false to not assign transit vehicles on the roads")]
+        public bool BackgroundTransit;
+
+        [RunParameter("On Road TTFs", "3-128", typeof(RangeSet), "The Transit Time Functions (TTFs) for transit segments that should be applied to the" +
+            " road links to reduce capacity for the buses and streetcars in mixed traffic.")]
+        public RangeSet OnRoadTTFs;
+
+        public sealed class Class : IModule
         {
-            char a = at[0];
-            if (at == "None" || at == "" || at == " ")
-            {
-                return "None";
-            }
-            if (a is '@')
-            {
-                return at;
-            }
-            else
-            {
-                return "@" + at;
-            }
-                
-        }
-        [RunParameter("Starting Node Number", 1, "Starting node range for the gate labels if they arent defined by a link extra attribute parameter")]
-        public int StartingNodeNumber;
+            [RunParameter("Mode", 'c', "The mode for this class.")]
+            public char Mode;
 
-        [RunParameter("Gate Labels", "None", "If you have a link extra attribute with your defined gate numbers, enter it here")]
-        public string GateLabel;
+            [RunParameter("Demand Matrix", 0, "The id of the demand matrix to use.")]
+            public int DemandMatrixNumber;
 
-        [SubModelInformation(Required = true, Description = "The SOLA parameters to perform the analysis")]
-        public SOLA _SOLA;
+            [RunParameter("Time Matrix", 0, "The matrix number to save in vehicle travel times")]
+            public int TimeMatrix;
 
-        public sealed class SOLA : IModule
-        {
+            [RunParameter("Cost Matrix", 0, "The matrix number to save the total cost into.")]
+            public int CostMatrix;
+
+            [RunParameter("Toll Matrix", 0, "The matrix to save the toll costs into.")]
+            public int TollMatrix;
+
+            [RunParameter("VolumeAttribute", "@auto_volume1", "The name of the attribute to save the volumes into (or None for no saving).")]
+            public string VolumeAttribute;
+
+            [RunParameter("TollAttributeID", "@toll", "The attribute containing the road tolls for this class of vehicle.")]
+            public string LinkTollAttributeID;
+
+            [RunParameter("Toll Weight", 0f, "")]
+            public float TollWeight;
+
+            [RunParameter("LinkCost", 0f, "The penalty in minutes per dollar to apply when traversing a link.")]
+            public float LinkCost;
+
             public string Name { get; set; }
 
             public float Progress { get; set; }
 
             public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
 
-            [SubModelInformation(Description = "The classes for the SOLA Road Assignment.")]
-            public Class[] Classes;
-
-            [RunParameter("Peak Hour Factor", 0f, "A factor to apply to the demand in order to build a representative hour.")]
-            public float PeakHourFactor;
-
-            [RunParameter("Iterations", 0, "The maximum number of iterations to run.")]
-            public int Iterations;
-
-            [RunParameter("Relative Gap", 0f, "The minimum gap required to terminate the algorithm.")]
-            public float RelativeGap;
-
-            [RunParameter("Best Relative Gap", 0f, "The minimum gap required to terminate the algorithm.")]
-            public float BestRelativeGap;
-
-            [RunParameter("Normalized Gap", 0f, "The minimum gap required to terminate the algorithm.")]
-            public float NormalizedGap;
-
-            [RunParameter("Performance Mode", true, "Set this to false to leave a free core for other work, recommended to leave set to true.")]
-            public bool PerformanceMode;
-
-            public sealed class Class : IModule
-            {
-                [RunParameter("Mode", 'c', "The mode for this class.")]
-                public char Mode;
-
-                [RunParameter("Demand Matrix", 0, "The id of the demand matrix to use.")]
-                public int DemandMatrixNumber;
-
-                [RunParameter("VolumeAttribute", "@auto_volume1", "The name of the attribute to save the volumes into (or None for no saving).")]
-                public string VolumeAttribute;
-
-                [RunParameter("TollAttributeID", "@toll", "The attribute containing the road tolls for this class of vehicle.")]
-                public string LinkTollAttributeID;
-
-                [RunParameter("Toll Weight", 0f, "")]
-                public float TollWeight;
-
-                [RunParameter("LinkCost", 0f, "The penalty in minutes per dollar to apply when traversing a link.")]
-                public float LinkCost;
-
-
-                public string Name { get; set; }
-
-                public float Progress { get; set; }
-
-                public Tuple<byte, byte, byte> ProgressColour { get { return new Tuple<byte, byte, byte>(50, 150, 50); } }
-
-                public bool RuntimeValidation(ref string error)
-                {
-                    if (Mode >= 'a' && Mode <= 'z' || Mode >= 'A' && Mode <= 'Z')
-                    {
-                        error = "In '" + Name + "' the Mode '" + Mode + "' is not a feasible mode for multi class assignment!";
-                        return true;
-                    }
-                    if (TollWeight <= 0.0000001)
-                    {
-                        error = "In '" + Name + "' the Toll Weight cannot be less than or equal to 0!";
-                        return true;
-                    }
-
-                    return false;
-
-
-                }
-            }
-
             public bool RuntimeValidation(ref string error)
             {
-                foreach (var c in Classes)
+                if (Mode >= 'a' && Mode <= 'z' || Mode >= 'A' && Mode <= 'Z')
                 {
-                    if (!c.RuntimeValidation(ref error))
-                    {
-                        return false;
-                    }
+                    error = "In '" + Name + "' the Mode '" + Mode + "' is not a feasible mode for multi class assignment!";
+                    return true;
                 }
-
-                return true;
+                if (TollWeight <= 0.0000001)
+                {
+                    error = "In '" + Name + "' the Toll Weight cannot be less than or equal to 0!";
+                    return true;
+                }
+                return false;
             }
-
         }
-        const string ToolName = "tmg.input_output.export_subarea_tool";
 
         public bool Execute(Controller controller)
         {
             var mc = controller as ModellerController;
             if (mc == null)
             {
-                throw new XTMFRuntimeException(this, "TMG.Emme.NetworkAssignment.MultiClassRoadAssignment requires the use of EMME Modeller and will not work through command prompt!");
+                throw new XTMFRuntimeException(this, "TMG.Emme.ExportSubareaTool requires the use of EMME Modeller and will not work through command prompt!");
             }
-
-
             string ret = null;
             if (!mc.CheckToolExists(this, ToolName))
             {
@@ -172,67 +145,67 @@ namespace TMG.Emme
 
         private ModellerControllerParameter[] GetParameters()
         {
-            /*
-                def __call__(self, xtmf_ScenarioNumber, xtmf_PeakHourFactor, xtmf_ModeList, xtmf_NameString, xtmf_AutoDemand, xtmf_LinkCosts, xtmf_LinkTollAttributeIds, 
-                 xtmf_TollWeights, xtmf_ResultAttributes, xtmf_MaxIterations, xtmf_rGap, xtmf_brGap, xtmf_normGap, xtmf_PerformanceFlag, 
-                 xtmf_ExtractTransitFlag, xtmf_SubareaNodeAttribute, xtmf_SubareaFolderPath, xtmf_NodeNumberStarting, xtmf_ShapefileLocation, xtmf_GateLabel):
-            */
             return new[]
             {
                 new ModellerControllerParameter("xtmf_ScenarioNumber", ScenarioNumber.ToString()),
-                new ModellerControllerParameter("xtmf_PeakHourFactor", _SOLA.PeakHourFactor.ToString()),
-                new ModellerControllerParameter("xtmf_ModeList", GetClasses()),
-                new ModellerControllerParameter("xtmf_NameString", string.Join(",", _SOLA.Classes.Select(c => c.Name))),
-                new ModellerControllerParameter("xtmf_AutoDemand", GetDemand()),
-                new ModellerControllerParameter("xtmf_LinkCosts", string.Join(",", _SOLA.Classes.Select(c => c.LinkCost.ToString()))),
-                new ModellerControllerParameter("xtmf_LinkTollAttributeIds", string.Join(",", _SOLA.Classes.Select(c => c.LinkTollAttributeID))),
-                new ModellerControllerParameter("xtmf_TollWeights", string.Join(",", _SOLA.Classes.Select(c => c.TollWeight.ToString()))),
-                new ModellerControllerParameter("xtmf_ResultAttributes", string.Join(",", _SOLA.Classes.Select(c => c.VolumeAttribute))),
-                new ModellerControllerParameter("xtmf_MaxIterations", _SOLA.Iterations.ToString()),
-                new ModellerControllerParameter("xtmf_rGap", _SOLA.RelativeGap.ToString()),
-                new ModellerControllerParameter("xtmf_brGap", _SOLA.BestRelativeGap.ToString()),
-                new ModellerControllerParameter("xtmf_normGap", _SOLA.NormalizedGap.ToString()),
-                new ModellerControllerParameter("xtmf_PerformanceFlag", _SOLA.PerformanceMode.ToString()),
-                new ModellerControllerParameter("xtmf_ExtractTransitFlag", ExtractTransit.ToString()),
-                new ModellerControllerParameter("xtmf_SubareaNodeAttribute", GetAttribute(SubareaNodeAttribute)),
-                new ModellerControllerParameter("xtmf_SubareaFolderPath", GetFileLocationOrNone(SubareaOutputFolder)),
-                new ModellerControllerParameter("xtmf_NodeNumberStarting", StartingNodeNumber.ToString()),
-                new ModellerControllerParameter("xtmf_ShapefileLocation", GetFileLocationOrNone(ShapefileLocation)),
-                new ModellerControllerParameter("xtmf_GateLabel", GetAttribute(GateLabel))
+                new ModellerControllerParameter("Mode_List", GetClasses()),
+                new ModellerControllerParameter("xtmf_Demand_String", GetDemand()),
+                new ModellerControllerParameter("TimesMatrixId", GetTimes()),
+                new ModellerControllerParameter("CostMatrixId", GetCosts()),
+                new ModellerControllerParameter("TollsMatrixId", GetTolls()),
+                new ModellerControllerParameter("PeakHourFactor", PeakHourFactor.ToString(CultureInfo.InvariantCulture)),
+                new ModellerControllerParameter("LinkCost", string.Join(",", Classes.Select(c => c.LinkCost.ToString(CultureInfo.InvariantCulture)))),
+                new ModellerControllerParameter("TollWeight", string.Join(",", Classes.Select(c => c.TollWeight.ToString(CultureInfo.InvariantCulture)))),
+                new ModellerControllerParameter("Iterations", Iterations.ToString(CultureInfo.InvariantCulture)),
+                new ModellerControllerParameter("rGap", RelativeGap.ToString(CultureInfo.InvariantCulture)),
+                new ModellerControllerParameter("brGap", BestRelativeGap.ToString(CultureInfo.InvariantCulture)),
+                new ModellerControllerParameter("normGap", NormalizedGap.ToString(CultureInfo.InvariantCulture)),
+                new ModellerControllerParameter("PerformanceFlag", PerformanceMode.ToString(CultureInfo.InvariantCulture)),
+                new ModellerControllerParameter("RunTitle", RunTitle),
+                new ModellerControllerParameter("LinkTollAttributeId", string.Join(",", Classes.Select(c => c.LinkTollAttributeID))),
+                new ModellerControllerParameter("xtmf_NameString", string.Join(",", Classes.Select(c => c.Name))),
+                new ModellerControllerParameter("ResultAttributes", string.Join(",", Classes.Select(c => c.VolumeAttribute))),
+                new ModellerControllerParameter("xtmf_BackgroundTransit", BackgroundTransit.ToString()),
+                new ModellerControllerParameter("OnRoadTTFRanges", OnRoadTTFs.ToString())
             };
+        }
+
+        private string GetTimes()
+        {
+            return string.Join(",", Classes.Select(c => "mf" + c.TimeMatrix.ToString()));
+        }
+
+        private string GetCosts()
+        {
+            return string.Join(",", Classes.Select(c => "mf" + c.CostMatrix.ToString()));
+        }
+
+        private string GetTolls()
+        {
+            return string.Join(",", Classes.Select(c => "mf" + c.TollMatrix.ToString()));
         }
 
         private string GetClasses()
         {
-            return string.Join(",", _SOLA.Classes.Select(c => c.Mode.ToString()));
+            return string.Join(",", Classes.Select(c => c.Mode.ToString()));
         }
 
         private string GetDemand()
         {
-            return string.Join(",", _SOLA.Classes.Select(c => "mf" + c.DemandMatrixNumber.ToString()));
-        }
-        private static string GetFileLocationOrNone(FileLocation location)
-        {
-            return location == null ? "None" : Path.GetFullPath(location.GetFilePath());
+            return string.Join(",", Classes.Select(c => "mf" + c.DemandMatrixNumber.ToString()));
         }
 
         public bool RuntimeValidation(ref string error)
         {
-            if (StartingNodeNumber < 1)
-            {
-                return false;
-            }
-            foreach (var c in _SOLA.Classes)
+            foreach (var c in Classes)
             {
                 if (!c.RuntimeValidation(ref error))
                 {
                     return false;
                 }
             }
-
             return true;
         }
     }
 
 }
-
