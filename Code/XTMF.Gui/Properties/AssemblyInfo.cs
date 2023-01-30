7c91b212cd281575175160abe2acc271fec58afb
James Vaughan <James.Vaughan@utoronto.ca>
2023-01-30 11:07:51 -0500

    Update references to XTMF 1.12. (#160)

--- Code/XTMF.Gui/Properties/AssemblyInfo.cs
+++ Code/XTMF.Gui/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XTMF.Gui.1.11")]
+[assembly: AssemblyTitle("XTMF.Gui.1.12")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("University of Toronto")]
-[assembly: AssemblyProduct("XTMF.Gui.1.10")]
-[assembly: AssemblyCopyright("Copyright © University of Toronto 2011-2022")]
+[assembly: AssemblyProduct("XTMF.Gui.1.12")]
+[assembly: AssemblyCopyright("Copyright © University of Toronto 2011-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.0")]
-[assembly: AssemblyFileVersion("1.11.0")]
+[assembly: AssemblyVersion("1.12.0")]
+[assembly: AssemblyFileVersion("1.12.0")]
