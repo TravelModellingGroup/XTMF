6dd130c9c0ac704089e1b8ec3bfefc9921cdca2f
James Vaughan <James.Vaughan@utoronto.ca>
2024-04-10 10:01:43 -0400

    Vectorized implementation of PoW (#252)

--- Code/XTMF.Gui/Properties/Settings.Designer.cs
+++ Code/XTMF.Gui/Properties/Settings.Designer.cs
@@ -8,18 +8,19 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace XTMF.Gui.Properties; 
-
-
-[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
-internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+namespace XTMF.Gui.Properties {
     
-    private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
     
-    public static Settings Default {
-        get {
-            return defaultInstance;
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
         }
     }
 }
