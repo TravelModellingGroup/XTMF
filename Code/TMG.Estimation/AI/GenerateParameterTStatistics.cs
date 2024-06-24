78ef769e9fa26af291ead89c93fb1eb428945b74
James Vaughan <phibred@hotmail.com>
2024-06-24 11:44:20 -0400

    Pushing current solution, has issues please do not integrate

--- Code/TMG.Estimation/AI/GenerateParameterTStatistics.cs
+++ Code/TMG.Estimation/AI/GenerateParameterTStatistics.cs
@@ -67,17 +67,28 @@ public class GenerateParameterTStatistics : IEstimationAI
     {
         var ret = new List<Job>();
         var parameters = Root.Parameters.ToArray();
-        using(var reader = new CsvReader(ResultFile.GetFilePath()))
+        using (var reader = new CsvReader(ResultFile.GetFilePath()))
         {
             int[] columnToParameterMap = CreateParameterMap(reader);
             var baseParameters = LoadBaseParameters(parameters, reader, columnToParameterMap);
             ret.Add(CreateZero(baseParameters));
             ret.Add(CreateJob(baseParameters));
-            for(int i = 0; i < baseParameters.Length; i++)
+            // Create the jobs for computing the diagonal of the Hessian
+            for (int i = 0; i < baseParameters.Length; i++)
             {
-                // we will need 4 points to approx the second derivative for now
+                // we will need 5 points to approx the second derivative for now
                 ret.Add(CreateWithOffset(baseParameters, i, -Delta));
+                ret.Add(CreateWithOffset(baseParameters, i, -Delta / 2));
+                ret.Add(CreateWithOffset(baseParameters, i, Delta / 2));
                 ret.Add(CreateWithOffset(baseParameters, i, Delta));
+            }
+            // Create the jobs for computing the off-diagonal of the Hessian
+            for (int i = 0; i < baseParameters.Length; i++)
+            {
+                for (int j = i + 1; j < baseParameters.Length; j++)
+                {
+                    ret.Add(CreateWithOffset(baseParameters, i, j, Delta));
+                }
             }
         }
         return ret;
@@ -86,7 +97,7 @@ public class GenerateParameterTStatistics : IEstimationAI
     private Job CreateZero(ParameterSetting[] baseParameters)
     {
         var parameters = Clone(baseParameters);
-        for(int i = 0; i < parameters.Length; i++)
+        for (int i = 0; i < parameters.Length; i++)
         {
             parameters[i].Current = parameters[i].NullHypothesis;
         }
@@ -100,10 +111,18 @@ public class GenerateParameterTStatistics : IEstimationAI
         return CreateJob(parameters);
     }
 
+    private Job CreateWithOffset(ParameterSetting[] baseParameters, int index, int index2, float delta)
+    {
+        var parameters = Clone(baseParameters);
+        parameters[index].Current += delta * (parameters[index].Maximum - parameters[index].Minimum);
+        parameters[index2].Current += delta * (parameters[index2].Maximum - parameters[index2].Minimum);
+        return CreateJob(parameters);
+    }
+
     private ParameterSetting[] Clone(ParameterSetting[] parameters)
     {
         ParameterSetting[] ret = new ParameterSetting[parameters.Length];
-        for(int i = 0; i < parameters.Length; i++)
+        for (int i = 0; i < parameters.Length; i++)
         {
             ret[i] = new ParameterSetting()
             {
@@ -158,22 +177,22 @@ public class GenerateParameterTStatistics : IEstimationAI
         var parameters = Root.Parameters.ToArray();
         reader.LoadLine(out int columns);
         var ret = new int[columns - 2];
-        for ( int i = 2; i < columns; i++ )
+        for (int i = 2; i < columns; i++)
         {
             reader.Get(out string name, i);
-            var selectedParameter = ( from p in parameters
-                                      where p.Names.Contains( name )
-                                      select p ).FirstOrDefault() ?? throw new XTMFRuntimeException(this, "In '" + Name + " the parameter '" + name + "' could not be resolved." );
-            ret[i - 2] = IndexOf( parameters, selectedParameter );
+            var selectedParameter = (from p in parameters
+                                     where p.Names.Contains(name)
+                                     select p).FirstOrDefault() ?? throw new XTMFRuntimeException(this, "In '" + Name + " the parameter '" + name + "' could not be resolved.");
+            ret[i - 2] = IndexOf(parameters, selectedParameter);
         }
         return ret;
     }
 
     private static int IndexOf(ParameterSetting[] parameters, ParameterSetting selectedParameter)
     {
-        for(int i = 0; i < parameters.Length; i++)
+        for (int i = 0; i < parameters.Length; i++)
         {
-            if(parameters[i] == selectedParameter) return i;
+            if (parameters[i] == selectedParameter) return i;
         }
         return -1;
     }
@@ -193,48 +212,169 @@ public class GenerateParameterTStatistics : IEstimationAI
         writer.Write(zeroValue);
         writer.Write(',');
         writer.WriteLine(GetRho(baseValue, zeroValue));
-        writer.WriteLine("ParameterName,Coefficient,LeftCoefficient,RightCoefficient,LeftFitness,RightFitness,SecondDerivative,t-statistic");
+        double[] tStatistics = ComputeTStatistics(jobs, parameters);
+        writer.WriteLine("ParameterName,Coefficient,NullHypothesis,TwoLeftCoefficient,LeftCoefficient,RightCoefficient,TwoRight,TwoLeftFitness,LeftFitness,RightFitness,TwoRightFitness,t-statistic");
         for (int i = 0; i < parameters.Count; i++)
         {
-            var secondDerivative = SecondDerivative(i);
+            var secondDerivative = SecondDerivativeForDiagonal(i);
             var current = jobs[1].Parameters[i].Current;
-            int offset = i * 2 + 2;
+            int offset = i * 4 + 2;
             writer.Write('"');
             writer.Write(parameters[i].Names[0]);
             writer.Write('"');
             writer.Write(',');
             writer.Write(current);
             writer.Write(',');
-            writer.Write(jobs[offset].Parameters[i].Current);
+            writer.Write(jobs[offset + 0].Parameters[i].NullHypothesis);
+            writer.Write(',');
+            writer.Write(jobs[offset + 0].Parameters[i].Current);
             writer.Write(',');
             writer.Write(jobs[offset + 1].Parameters[i].Current);
             writer.Write(',');
-            writer.Write(jobs[offset].Value);
+            writer.Write(jobs[offset + 2].Parameters[i].Current);
+            writer.Write(',');
+            writer.Write(jobs[offset + 3].Parameters[i].Current);
+            writer.Write(',');
+            writer.Write(jobs[offset + 0].Value);
             writer.Write(',');
             writer.Write(jobs[offset + 1].Value);
             writer.Write(',');
-            writer.Write(secondDerivative);
+            writer.Write(jobs[offset + 2].Value);
             writer.Write(',');
-            writer.WriteLine(ComputeTStatistic(current, secondDerivative));
+            writer.Write(jobs[offset + 3].Value);
+            writer.Write(',');
+            writer.Write(tStatistics[i]);
+            writer.WriteLine();
         }
     }
 
-    private double ComputeTStatistic(float current, double secondDerivative)
+    private double[] ComputeTStatistics(List<Job> jobs, List<ParameterSetting> parameters)
     {
-        var variance = (Maximize ? -1.0f : 1.0f) / secondDerivative;
-        var std = Math.Sqrt(variance);
-        return current / std;
+        double[] ret = new double[parameters.Count];
+
+        // Step 1) Compute the Fisher Information Matrix
+        double[,] fisherMatrix = new double[parameters.Count, parameters.Count];
+        for (int i = 0; i < parameters.Count; i++)
+        {
+            fisherMatrix[i, i] = -SecondDerivativeForDiagonal(i);
+        }
+        for (int i = 0; i < parameters.Count; i++)
+        {
+            for (int j = i + 1; j < parameters.Count; j++)
+            {
+                fisherMatrix[i, j] = -SecondDerivativeForOffDiagonal(parameters, jobs, i, j);
+            }
+        }
+        // Copy the upper half to the lower half
+        for (int i = 0; i < parameters.Count; i++)
+        {
+            for (int j = i + 1; j < parameters.Count; j++)
+            {
+                fisherMatrix[j, i] = fisherMatrix[i, j];
+            }
+        }
+
+        WriteMatrix(parameters, fisherMatrix, "FisherMatrix.csv");
+
+        // Step 2) Compute the inverse of the Fisher Information Matrix
+
+        double[,] inverseFisherMatrix = Inverse(fisherMatrix);
+
+        WriteMatrix(parameters, inverseFisherMatrix, "InverseFisherMatrix.csv");
+
+        // Step 3) Compute the t-statistics
+
+        for (int i = 0; i < parameters.Count; i++)
+        {
+            ret[i] = (jobs[1].Parameters[i].Current - jobs[1].Parameters[i].NullHypothesis) / Math.Sqrt(inverseFisherMatrix[i, i]);
+        }
+
+        return ret;
     }
 
-    private double SecondDerivative(int parameterIndex)
+    private void WriteMatrix(List<ParameterSetting> parameters, double[,] fisherMatrix, string fileName)
+    {
+        using var writer = new StreamWriter(fileName);
+        for (int i = 0; i < fisherMatrix.GetLength(0); i++)
+        {
+            if(i != 0)
+            {
+                writer.Write(',');
+            }
+            writer.Write(parameters[i].Names[i]);
+        }
+        for (int i = 0; i < fisherMatrix.GetLength(0); i++)
+        {
+            writer.Write(parameters[i].Names[i]);
+            for (int j = 0; j < fisherMatrix.GetLength(1); j++)
+            {
+                writer.Write(',');
+                writer.Write(fisherMatrix[i, j]);
+            }
+            writer.WriteLine();
+        }
+    }
+
+    private static double GetPlusDeltaFor(List<Job> jobs, int parameterIndex)
+    {
+        return jobs[parameterIndex * 4 + 2 + 4].Value;
+    }
+
+    private static double GetPlusDeltaFor(List<Job> jobs, int totalParameters, int parameterIndex, int parameterIndex2)
+    {
+        var additionalOffset = ComputeTopHalfIndex(parameterIndex, parameterIndex2, totalParameters);
+        return jobs[totalParameters * 4 
+            + additionalOffset]
+            .Value;
+    }
+
+    private static int ComputeTopHalfIndex(int parameterIndex, int parameterIndex2, int totalParameters)
+    {
+        int n = totalParameters - 1;
+        int index = parameterIndex * (n + 1) - (parameterIndex * (parameterIndex + 1)) / 2 + parameterIndex2 - parameterIndex - 1;
+        return index;
+    }
+
+    private double SecondDerivativeForDiagonal(int parameterIndex)
     {
         var jobs = Root.CurrentJobs;
         var parameters = Root.Parameters;
         var parameterDelta = Delta * (parameters[parameterIndex].Maximum - parameters[parameterIndex].Minimum);
-        // 4 parameters per job, 2 jobs to get value and zero value
-        var parameterOffset = parameterIndex * 2 + 2;
-        return (jobs[parameterOffset + 1].Value - (2 * jobs[1].Value) + jobs[parameterOffset].Value)
-            / (parameterDelta * parameterDelta);
+        // 5 parameters per job (4 for second derivative and the first for the null hypothesis), 2 jobs to get value and zero value
+        var parameterOffset = parameterIndex * 4 + 2;
+
+        var yn2 = jobs[parameterOffset + 0].Value;
+        var yn1 = jobs[parameterOffset + 1].Value;
+        // Job 0 is the zero parameters model, job 1 is the full parameters model
+        var y0 = jobs[1].Value;
+        var y1 = jobs[parameterOffset + 2].Value;
+        var y2 = jobs[parameterOffset + 3].Value;
+        // The points are spaced out by half of the delta
+        return SecondDerivativeFivePoints(yn2, yn1, y0, y1, y2, parameterDelta / 2.0);
+    }
+
+    private double GetDeltaForParameter(int parameterIndex)
+    {
+        var parameters = Root.Parameters;
+        return Delta * (parameters[parameterIndex].Maximum - parameters[parameterIndex].Minimum);
+    }
+
+    private double SecondDerivativeForOffDiagonal(List<ParameterSetting> parameters, List<Job> jobs, int i, int j)
+    {
+        double original = jobs[1].Value;
+        double plusDeltaI = GetPlusDeltaFor(jobs, i);
+        double plusDeltaJ = GetPlusDeltaFor(jobs, j);
+        double plusDeltaBoth = GetPlusDeltaFor(jobs, parameters.Count, i, j);
+
+        double secondDerivative = (plusDeltaBoth - plusDeltaI - plusDeltaJ + original) / (GetDeltaForParameter(i) * GetDeltaForParameter(j));
+        return secondDerivative;
+    }
+
+    private static double SecondDerivativeFivePoints(double yn2, double yn1, double y0, double y1, double y2, double deltaX)
+    {
+        var numerator = -yn2 + 16 * yn1 - 30 * y0 + 16 * y1 - y2;
+        var denominator = 12 * deltaX * deltaX;
+        return numerator / denominator;
     }
 
     private float GetRho(float current, float zeroParams)
@@ -246,4 +386,67 @@ public class GenerateParameterTStatistics : IEstimationAI
     {
         return true;
     }
+
+    private static double[,] Inverse(double[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        double[,] result = new double[n, n];
+        double[,] augmented = new double[n, 2 * n];
+        int i, j, k;
+        double temp;
+
+        // Initialize the result matrix as an identity matrix
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (i == j) result[i, j] = 1;
+                else result[i, j] = 0;
+            }
+        }
+
+        // Copy the input matrix to the augmented matrix
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                augmented[i, j] = matrix[i, j];
+            }
+        }
+
+        // Perform the Gauss-Jordan elimination
+        for (i = 0; i < n; i++)
+        {
+            // Make the diagonal contain all 1's
+            temp = augmented[i, i];
+            for (j = 0; j < 2 * n; j++)
+            {
+                augmented[i, j] /= temp;
+            }
+
+            // Make the rest of the column contain all 0's
+            for (j = 0; j < n; j++)
+            {
+                if (i != j)
+                {
+                    temp = augmented[j, i];
+                    for (k = 0; k < 2 * n; k++)
+                    {
+                        augmented[j, k] -= augmented[i, k] * temp;
+                    }
+                }
+            }
+        }
+
+        // Extract the inverse matrix from the augmented matrix
+        for (i = 0; i < n; i++)
+        {
+            for (j = n; j < 2 * n; j++)
+            {
+                result[i, j - n] = augmented[i, j];
+            }
+        }
+        return result;
+    }
+
 }
