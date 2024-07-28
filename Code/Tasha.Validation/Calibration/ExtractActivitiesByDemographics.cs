e4117804b14451d5d6c09a998f6c0b30ec6b47dd
James Vaughan <phibred@hotmail.com>
2024-07-28 18:53:49 -0400

    Added the ability to select and reject based on person attributes.

--- Code/Tasha.Validation/Calibration/ExtractActivitiesByDemographics.cs
+++ Code/Tasha.Validation/Calibration/ExtractActivitiesByDemographics.cs
@@ -42,6 +42,7 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
     [RunParameter("Age Ranges", "0-200", typeof(RangeSet), "The valid ages to get.", Index = 3)]
     public RangeSet AgeRanges;
 
+    [ModuleInformation(Description = "Specify which activities will be recorded.")]
     public sealed class SelectedActivity : IModule
     {
         [RunParameter("Activity", Activity.PrimaryWork, "An activity type to include.")]
@@ -57,10 +58,90 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
         {
             return true;
         }
+
+    }
+
+    [ModuleInformation(Description = "Specify which occupations will be selected.")]
+    public sealed class SelectedOccupation : IModule
+    {
+        [RunParameter("Occupation", Occupation.Professional, "An occupation to include.")]
+        public Occupation Occupation;
+
+        public string Name { get; set; }
+
+        public float Progress => 0f;
+
+        public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+
+    }
+
+    [ModuleInformation(Description = "Specify which employments statuses will be selected for.")]
+    public sealed class SelectedEmploymentStatus : IModule
+    {
+        [RunParameter("Employment Status", TTSEmploymentStatus.FullTime, "An employment status to include.")]
+        public TTSEmploymentStatus EmploymentStatus;
+
+        public string Name { get; set; }
+        public float Progress => 0f;
+        public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
+
+    }
+
+    [ModuleInformation(Description = "Specify which status statuses will be selected for.")]
+    public sealed class SelectedStudentStatuses : IModule
+    {
+        [RunParameter("Student Status", StudentStatus.FullTime, "A student status to include.")]
+        public StudentStatus StudentStatus;
+
+        public string Name { get; set; }
+
+        public float Progress => 0f;
+
+        public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+
+        public bool RuntimeValidation(ref string error)
+        {
+            return true;
+        }
     }
 
     [SubModelInformation(Required = true, Description = "The activities to add to the matrix.")]
     public SelectedActivity[] Activities;
+    private Activity[] _activities;
+
+    [SubModelInformation(Required = false, Description = "The occupations to select for, leave blank for all.")]
+    public SelectedOccupation[] SelectOccupations;
+    private Occupation[] _occupations;
+
+    [SubModelInformation(Required = false, Description = "The employment statuses to select for, leave blank for all.")]
+    public SelectedEmploymentStatus[] SelectEmploymentStatuses;
+    private TTSEmploymentStatus[] _employmentStatuses;
+
+    [SubModelInformation(Required = false, Description = "The student statuses to select for, leave blank for all.")]
+    public SelectedStudentStatuses[] SelectStudentStatuses;
+    private StudentStatus[] _studentStatuses;
+
+    [SubModelInformation(Required = false, Description = "The occupations to reject.")]
+    public SelectedOccupation[] RejectOccupations;
+    private Occupation[] _rejectOccupations;
+
+    [SubModelInformation(Required = false, Description = "The employment statuses to reject.")]
+    public SelectedEmploymentStatus[] RejectEmploymentStatuses;
+    private TTSEmploymentStatus[] _rejectEmploymentStatuses;
+
+    [SubModelInformation(Required = false, Description = "The student statuses to select to reject.")]
+    public SelectedStudentStatuses[] RejectStudentStatuses;
+    private StudentStatus[] _rejectStudentStatuses;
 
     [RootModule]
     public ITravelDemandModel Root;
@@ -80,8 +161,19 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
     {
         _targetIteration = maxIterations - 1;
         _containedActivities = Activities.Select(a => a.Activity).ToArray();
+        _activities = Activities.Select(a => a.Activity).ToArray();
+        _occupations = SelectOccupations?.Select(o => o.Occupation).ToArray() ?? [];
+        _employmentStatuses = SelectEmploymentStatuses?.Select(e => e.EmploymentStatus).ToArray() ?? [];
+        _studentStatuses = SelectStudentStatuses?.Select(s => s.StudentStatus).ToArray() ?? [];
+        _rejectOccupations = RejectOccupations?.Select(o => o.Occupation).ToArray() ?? [];
+        _rejectEmploymentStatuses = RejectEmploymentStatuses?.Select(e => e.EmploymentStatus).ToArray() ?? [];
+        _rejectStudentStatuses = RejectStudentStatuses?.Select(s => s.StudentStatus).ToArray() ?? [];
     }
 
+    /// <summary>
+    /// Called when the iteration is starting.
+    /// </summary>
+    /// <param name="iteration">The current iteration.</param>
     public void IterationStarting(int iteration)
     {
         if (_targetIteration != iteration)
@@ -106,6 +198,11 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
 
     private readonly record struct Entry(int FlatOrigin, int FlatDestination, float ExpansionFactor);
 
+    /// <summary>
+    /// Executes the logic to extract activities by demographics for a given household and iteration.
+    /// </summary>
+    /// <param name="household">The household to extract activities for.</param>
+    /// <param name="iteration">The current iteration.</param>
     public void Execute(ITashaHousehold household, int iteration)
     {
         if (_targetIteration != iteration)
@@ -116,7 +213,6 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
         int numberOfEntries = 0;
         int maxEntries = household.Persons.Length * 10;
         Span<Entry> entries = stackalloc Entry[maxEntries];
-
 
         void WriteEntries(Span<Entry> entries)
         {
@@ -149,6 +245,14 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
         // Only do work on the last iteration
         foreach (var person in household.Persons)
         {
+            if (!AgeRanges.Contains(person.Age)
+                    || !IsSelected(_occupations, _rejectOccupations, person.Occupation)
+                    || !IsSelected(_employmentStatuses, _rejectEmploymentStatuses, person.EmploymentStatus)
+                    || !IsSelected(_studentStatuses, _rejectStudentStatuses, person.StudentStatus)
+               )
+            {
+                continue;
+            }
             var expansionFactor = person.ExpansionFactor;
             foreach (var tripChain in person.TripChains)
             {
@@ -156,8 +260,7 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
                 {
                     Time startTime = GetStartTime(trip);
                     if (Array.IndexOf(_containedActivities, trip.Purpose) >= 0
-                        && startTime >= StartTime && startTime < EndTime
-                        && AgeRanges.Contains(person.Age))
+                        && startTime >= StartTime && startTime < EndTime)
                     {
                         AddEntry(entries, trip.OriginalZone, trip.DestinationZone, expansionFactor);
                     }
@@ -166,6 +269,27 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
         }
 
         WriteEntries(entries);
+    }
+
+    /// <summary>
+    /// Checks if a value is selected based on the provided selected and rejected arrays.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="selected">The array of selected values.</param>
+    /// <param name="rejected">The array of rejected values.</param>
+    /// <param name="value">The value to check.</param>
+    /// <returns>True if the value is selected, false otherwise.</returns>
+    private static bool IsSelected<T>(T[] selected, T[] rejected, T value)
+    {
+        if (selected is not null && selected.Length > 0)
+        {
+            return Array.IndexOf(selected, value) >= 0;
+        }
+        if (rejected is not null && rejected.Length > 0)
+        {
+            return Array.IndexOf(rejected, value) < 0;
+        }
+        return true;
     }
 
     public void IterationFinished(int iteration)
@@ -188,7 +312,7 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
 
     public string Name { get; set; }
 
-    public float Progress => throw new NotImplementedException();
+    public float Progress => 0f;
 
     public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
 
@@ -196,4 +320,5 @@ public sealed class ExtractActivitiesByDemographics : IPostHousehold
     {
         return true;
     }
+
 }
