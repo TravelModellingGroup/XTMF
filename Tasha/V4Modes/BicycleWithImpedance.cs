c17e70b446470022ee8466ff7a4c5956bbb424b3
James Vaughan <James.Vaughan@utoronto.ca>
2025-05-22 09:59:14 -0400

    Add new bicycle mode that allows different utilities by time period. (#323)
    * Add new bicycle mode that allows different utilities by time period.

---
+++ Tasha/V4Modes/BicycleWithImpedance.cs
@@ -0,0 +1,537 @@
+﻿/*
+    Copyright 2025 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using Datastructure;
+using System;
+using System.Threading.Tasks;
+using Tasha.Common;
+using TMG;
+using XTMF;
+
+namespace Tasha.V4Modes;
+
+/// <summary>
+/// Provides the ability to ride a bike
+/// </summary>
+[ModuleInformation(Description =
+    @"This module is designed to implement the Bicycle mode for GTAModel V4.0+ that now takes in an additional custom utility
+distance and travel times by given time period.")]
+public sealed class BicycleWithImpedance : ITashaMode, IIterationSensitive
+{
+    [RootModule]
+    public ITashaRuntime Root;
+
+    [RunParameter("Constant", 0f, "The constant factor applied for the bicycle mode")]
+    public float Constant;
+
+    [RunParameter("MarketFlag", 0f, "Added to the utility if the trip's purpose is market.")]
+    public float MarketFlag;
+
+    [RunParameter("OtherFlag", 0f, "Added to the utility if the trip's purpose is 'other'.")]
+    public float OtherFlag;
+
+    [RunParameter("SchoolFlag", 0f, "Added to the utility if the trip's purpose is 'School'.")]
+    public float SchoolFlag;
+
+    [RunParameter("WorkFlag", 0f, "Added to the utility if the trip's purpose is 'Primary Work, Secondary Work, or Work Based Business'.")]
+    public float WorkFlag;
+
+    [RunParameter("IntrazonalConstant", 0f, "The factor applied for being an intrazonal trip")]
+    public float IntrazonalConstant;
+
+    [RunParameter("Max Travel Distance", 12000f, "The largest distance a person is allowed to bike (meters)")]
+    public float MaxTravelDistance;
+
+    [RunParameter("ProfessionalTimeFactor", 0f, "The TimeFactor applied to the person type.")]
+    public float ProfessionalTimeFactor;
+
+    [RunParameter("GeneralTimeFactor", 0f, "The TimeFactor applied to the person type.")]
+    public float GeneralTimeFactor;
+
+    [RunParameter("SalesTimeFactor", 0f, "The TimeFactor applied to the person type.")]
+    public float SalesTimeFactor;
+
+    [RunParameter("ManufacturingTimeFactor", 0f, "The TimeFactor applied to the person type.")]
+    public float ManufacturingTimeFactor;
+
+    [RunParameter("StudentTimeFactor", 0f, "The TimeFactor applied to the person type.")]
+    public float StudentTimeFactor;
+
+    [RunParameter("NonWorkerStudentTimeFactor", 0f, "The TimeFactor applied to the person type.")]
+    public float NonWorkerStudentTimeFactor;
+
+    [DoNotAutomate]
+    public IVehicleType VehicleType = null;
+
+    [RunParameter("YoungAdultFlag", 0f, "The factor applied for being a young adult")]
+    public float YoungAdultFlag;
+
+    [RunParameter("YouthFlag", 0f, "The factor applied for the being a youth")]
+    public float YouthFlag;
+
+    [RunParameter("ChildFlag", 0f, "The factor applied for the being a child")]
+    public float ChildFlag;
+
+    [RunParameter("ProfessionalConstant", 0f, "The constant applied to the person type.")]
+    public float ProfessionalConstant;
+
+    [RunParameter("GeneralConstant", 0f, "The constant applied to the person type.")]
+    public float GeneralConstant;
+
+    [RunParameter("SalesConstant", 0f, "The constant applied to the person type.")]
+    public float SalesConstant;
+
+    [RunParameter("ManufacturingConstant", 0f, "The constant applied to the person type.")]
+    public float ManufacturingConstant;
+
+    [RunParameter("StudentConstant", 0f, "The constant applied to the person type.")]
+    public float StudentConstant;
+
+    [RunParameter("NonWorkerStudentConstant", 0f, "The constant applied to the person type.")]
+    public float NonWorkerStudentConstant;
+
+    [RunParameter("Mode Name", "Bicycle", "The name of the mode")]
+    public string ModeName { get; set; }
+
+    public float CurrentlyFeasible { get; set; } = 1.0f;
+
+    [SubModelInformation(Required = false, Description = "Constants for time of day")]
+    public TimePeriodSpatialConstant[] TimePeriodConstants;
+
+    public sealed class TimePeriodData : IModule
+    {
+
+        [RunParameter("Start Time", "6:00", typeof(Time), "The start time for this data.", Index = 0)]
+        public Time StartTime;
+
+        [RunParameter("End Time", "9:00", typeof(Time), "The start time for this data.", Index = 1)]
+        public Time EndTime;
+
+        [SubModelInformation(Required = true, Description = "The time, in minutes, it takes to go from origin to destination.")]
+        public IDataSource<SparseTwinIndex<float>> TravelTime;
+
+        [SubModelInformation(Required = true, Description = "The distance, in meters, to go from the origin to destination.")]
+        public IDataSource<SparseTwinIndex<float>> Distance;
+
+        [SubModelInformation(Required = true, Description = "Utility to go from the origin to destination.")]
+        public IDataSource<SparseTwinIndex<float>> Utility;
+
+        internal SparseTwinIndex<float> _travelTime;
+
+        internal SparseTwinIndex<float> _distances;
+
+        internal SparseTwinIndex<float> _utility;
+
+        public string Name { get; set; }
+
+        public float Progress => 0f;
+
+        public Tuple<byte, byte, byte> ProgressColour => new (50, 150, 50);
+
+        public void Load()
+        {
+            Parallel.Invoke(
+                () => _travelTime = GetMatrix(TravelTime),
+                () => _distances = GetMatrix(Distance),
+                () => _utility = GetMatrix(Utility)
+            );
+        }
+
+        public void Unload()
+        {
+            _travelTime = null;
+            _distances = null;
+            _utility = null;
+        }
+
+        private static SparseTwinIndex<float> GetMatrix(IDataSource<SparseTwinIndex<float>> dataSource)
+        {
+            dataSource.LoadData();
+            var ret = dataSource.GiveData();
+            dataSource.UnloadData();
+            return ret;
+        }
+
+        public bool RuntimeValidation(ref string error)
+        {
+            if (StartTime >= EndTime)
+            {
+                error = "The start time must be before the end time.";
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [SubModelInformation(Required = true, Description = "Travel times, utility, and distance by time of activity.")]
+    public TimePeriodData[] PeriodData;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public string Name { get; set; }
+
+    public string NetworkType => string.Empty;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public bool NonPersonalVehicle => false;
+    
+
+    [RunParameter("Observed Signature Code", 'A', "The character code used for model output.")]
+    public char ObservedMode
+    {
+        get;
+        set;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public char OutputSignature
+    {
+        get;
+        set;
+    }
+
+    public float Progress => 0;
+
+    public Tuple<byte, byte, byte> ProgressColour => new(50, 150, 50);
+
+    /// <summary>
+    ///
+    /// </summary>
+    [DoNotAutomate]
+    public IVehicleType RequiresVehicle => null;
+
+    [RunParameter("Variance Scale", 1.0f, "The scale for variance used for variance testing.")]
+    public double VarianceScale { get; set; }
+
+    [SubModelInformation(Required = false, Description = "An optional utility modification when traveling to the given zone.")]
+    public IDataSource<SparseArray<float>> ZonalDestinationUtility;
+
+    private float[] _zonalDestinationUtility;
+
+    private SparseArray<IZone> _zoneSystem;
+
+    /// <summary>
+    /// The V for a trip whose mode is bike
+    /// </summary>
+    /// <param name="trip">The trip</param>
+    /// <returns>The V for this trip</returns>
+    public double CalculateV(ITrip trip)
+    {
+        float v = 0;
+        ITashaPerson person = trip.TripChain.Person;
+        GetPersonVariables(person, out float timeFactor, out float constant);
+        v += constant;
+        Time startTime = trip.ActivityStartTime;
+        IZone origin = trip.OriginalZone;
+        IZone destination = trip.DestinationZone;
+        var o = _zoneSystem.GetFlatIndex(origin.ZoneNumber);
+        var d = _zoneSystem.GetFlatIndex(destination.ZoneNumber);
+        var periodData = GetData(startTime);
+        v += periodData._utility.GetFlatData()[o][d];
+        v += _customUtility?[o][d] ?? 0;
+        if (o == d)
+        {
+            v += IntrazonalConstant;
+        }
+        v += timeFactor * TravelTime(o, d, startTime).ToMinutes();
+        v += _zonalDestinationUtility[d];
+        if (person.Youth)
+        {
+            v += YouthFlag;
+        }
+        if (person.YoungAdult)
+        {
+            v += YoungAdultFlag;
+        }
+        v += trip.Purpose switch
+        {
+            Activity.Market or Activity.JointMarket => MarketFlag,
+            Activity.JointOther or Activity.IndividualOther => OtherFlag,
+            Activity.School => SchoolFlag,
+            Activity.PrimaryWork
+            or Activity.WorkBasedBusiness
+            or Activity.SecondaryWork => WorkFlag,
+            _ => 0
+        };
+        return v + GetPlanningDistrictConstant(startTime, origin.PlanningDistrict, destination.PlanningDistrict);
+    }
+
+    public float GetPlanningDistrictConstant(Time startTime, int pdO, int pdD)
+    {
+        for (int i = 0; i < TimePeriodConstants.Length; i++)
+        {
+            if (startTime >= TimePeriodConstants[i].StartTime && startTime < TimePeriodConstants[i].EndTime)
+            {
+                return TimePeriodConstants[i].GetConstant(pdO, pdD);
+            }
+        }
+        return 0f;
+    }
+
+    private void GetPersonVariables(ITashaPerson person, out float time, out float constant)
+    {
+        if (person.EmploymentStatus == TTSEmploymentStatus.FullTime)
+        {
+            switch (person.Occupation)
+            {
+                case Occupation.Professional:
+                    constant = ProfessionalConstant;
+                    time = ProfessionalTimeFactor;
+                    return;
+                case Occupation.Office:
+                    constant = GeneralConstant;
+                    time = GeneralTimeFactor;
+                    return;
+                case Occupation.Retail:
+                    constant = SalesConstant;
+                    time = SalesTimeFactor;
+                    return;
+                case Occupation.Manufacturing:
+                    constant = ManufacturingConstant;
+                    time = ManufacturingTimeFactor;
+                    return;
+            }
+        }
+        switch (person.StudentStatus)
+        {
+            case StudentStatus.FullTime:
+            case StudentStatus.PartTime:
+                constant = StudentConstant;
+                time = StudentTimeFactor;
+                return;
+        }
+        if (person.EmploymentStatus == TTSEmploymentStatus.PartTime)
+        {
+            switch (person.Occupation)
+            {
+                case Occupation.Professional:
+                    constant = ProfessionalConstant;
+                    time = ProfessionalTimeFactor;
+                    return;
+                case Occupation.Office:
+                    constant = GeneralConstant;
+                    time = GeneralTimeFactor;
+                    return;
+                case Occupation.Retail:
+                    constant = SalesConstant;
+                    time = SalesTimeFactor;
+                    return;
+                case Occupation.Manufacturing:
+                    constant = ManufacturingConstant;
+                    time = ManufacturingTimeFactor;
+                    return;
+            }
+        }
+        constant = NonWorkerStudentConstant;
+        time = NonWorkerStudentTimeFactor;
+    }
+
+    /// <summary>
+    /// Calculate the utility of moving between and origin and a destination
+    /// with a disregard for the person taking it
+    /// </summary>
+    /// <param name="origin">Where they start</param>
+    /// <param name="destination">where they go</param>
+    /// <param name="time">What time the trip started at</param>
+    /// <returns>The number of minutes it takes</returns>
+    public float CalculateV(IZone origin, IZone destination, Time time)
+    {
+        throw new NotImplementedException("The interface for trip based models is not supported at this time.");
+    }
+
+    /// <summary>
+    /// Returns the cost of riding a bike
+    /// </summary>
+    /// <param name="origin">The origin zone</param>
+    /// <param name="destination">The destination zone</param>
+    /// <param name="time"></param>
+    /// <returns>the cost</returns>
+    public float Cost(IZone origin, IZone destination, Time time)
+    {
+        return 0;
+    }
+
+    public bool Feasible(IZone origin, IZone destination, Time timeOfDay)
+    {
+        var data = GetData(timeOfDay);
+        return data._distances[origin.ZoneNumber, destination.ZoneNumber] <= MaxTravelDistance;
+    }
+
+    [RunParameter("Default Time Period", 0, "The time period to use if outside of all defined time periods.")]
+    public int DefaultTimePeriod;
+
+    private TimePeriodData GetData(Time time)
+    {
+        foreach (var period in PeriodData)
+        {
+            if (time >= period.StartTime && time < period.EndTime)
+            {
+                return period;
+            }
+        }
+        return PeriodData[DefaultTimePeriod];
+    }
+
+    /// <summary>
+    /// Checking Bike Feasibility
+    /// </summary>
+    /// <param name="trip">The Trip</param>
+    /// <returns>is it Feasible?</returns>
+    public bool Feasible(ITrip trip)
+    {
+        var data = GetData(trip.ActivityStartTime);
+        return data._distances[trip.OriginalZone.ZoneNumber, trip.DestinationZone.ZoneNumber] <= MaxTravelDistance;
+    }
+
+    /// <summary>
+    /// Checking feasibility of entire trip chain for a bike
+    /// </summary>
+    /// <param name="tripChain">The Trip Chain to test feasibility on</param>
+    /// <returns>is this trip chain feasible?</returns>
+    public bool Feasible(ITripChain tripChain)
+    {
+        var trips = tripChain.Trips;
+        var lastPlace = trips[0].OriginalZone;
+        var homeZone = lastPlace;
+        bool lastMadeWithBike = true;
+        bool firstWasBike = trips[0].Mode == this;
+        bool anyBike = false;
+        for (int i = 0; i < trips.Count; i++)
+        {
+            var trip = trips[i];
+            if (trip.Mode == this)
+            {
+                anyBike = true;
+                if (trip.OriginalZone != lastPlace)
+                {
+                    return false;
+                }
+                lastPlace = trip.DestinationZone;
+                lastMadeWithBike = true;
+            }
+            else
+            {
+                lastMadeWithBike = false;
+            }
+        }
+        return !anyBike | (firstWasBike & lastPlace == homeZone & lastMadeWithBike);
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="observedMode"></param>
+    /// <returns></returns>
+    public bool IsObservedMode(char observedMode)
+    {
+        return (observedMode == ObservedMode);
+    }
+
+    /// <summary>
+    /// This is called before the start method as a way to pre-check that all of the parameters that are selected
+    /// are in fact valid for this module.
+    /// </summary>
+    /// <param name="error">A string that should be assigned a detailed error</param>
+    /// <returns>If the validation was successful or if there was a problem</returns>
+    public bool RuntimeValidation(ref string error)
+    {
+        if(DefaultTimePeriod < 0 || DefaultTimePeriod >= PeriodData.Length)
+        {
+            error = "The default time period is not valid!";
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// The Travel time from an origin to a zone using a bike
+    /// </summary>
+    /// <param name="origin">The origin zone</param>
+    /// <param name="destination">The destination zone</param>
+    /// <param name="time">The time of day</param>
+    /// <returns>The travel time</returns>
+    public Time TravelTime(IZone origin, IZone destination, Time time)
+    {
+        var data = GetData(time);
+        var travelTime = data._travelTime[origin.ZoneNumber, destination.ZoneNumber];
+        return Time.FromMinutes(travelTime);
+    }
+
+    private Time TravelTime(int flatOrigin, int flatDesination, Time time)
+    {
+        var data = GetData(time);
+        var travelTime = data._travelTime.GetFlatData()[flatOrigin][flatDesination];
+        return Time.FromMinutes(travelTime);
+    }
+
+    public void IterationEnding(int iterationNumber, int maxIterations)
+    {
+        foreach (var period in PeriodData)
+        {
+            period.Unload();
+        }
+    }
+
+    [SubModelInformation(Required = false, Description = "An optional custom utility matrix to apply to the mode.")]
+    public IDataSource<SparseTwinIndex<float>> CustomUtility;
+    private float[][] _customUtility;
+
+    public void IterationStarting(int iterationNumber, int maxIterations)
+    {
+        _zoneSystem = Root.ZoneSystem.ZoneArray;
+        
+        for (int i = 0; i < TimePeriodConstants.Length; i++)
+        {
+            TimePeriodConstants[i].BuildMatrix();
+        }
+        if (ZonalDestinationUtility != null)
+        {
+            ZonalDestinationUtility.LoadData();
+            _zonalDestinationUtility = ZonalDestinationUtility.GiveData().GetFlatData();
+            ZonalDestinationUtility.UnloadData();
+            if (_zonalDestinationUtility.Length != _zoneSystem.Count)
+            {
+                throw new XTMFRuntimeException(this, $"The number of zones in the mode's ZonalUtility is not consistent with the current zone system!");
+            }
+        }
+        else
+        {
+            if (_zonalDestinationUtility == null || _zonalDestinationUtility.Length != _zoneSystem.Count)
+            {
+                _zonalDestinationUtility = new float[_zoneSystem.Count];
+            }
+        }
+        foreach(var period in PeriodData)
+        {
+            period.Load();
+        }
+        _customUtility = null;
+        if (CustomUtility is not null)
+        {
+            CustomUtility.LoadData();
+            _customUtility = CustomUtility.GiveData()!.GetFlatData();
+            CustomUtility.UnloadData();
+        }
+    }
+
+}
